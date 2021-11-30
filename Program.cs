3f779b1a56842a35b97628751a3626e84494258e
302Blue <abaldwin@udel.edu>
2021-11-29 20:12:35 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Endevor {
+    class Program {
+        static void Main(string[] args) {
+            string Str = "Hello World";
+            Console.WriteLine(Str);
+        }
+    }
+    
+}
